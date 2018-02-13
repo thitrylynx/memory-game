c242e376ec838c76a8959ccbbacfb96ece4efc33
Vyacheslav <thirtylynx@gmail.com>
2018-02-13 21:09:23 +0500

    Initial function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+var startPage = document.querySelector('.start-page');
+var gamePage = document.querySelector('.game-page');
+var startButton = startPage.querySelector('.start-page_button');
+
+startButton.addEventListener('click', function() {
+  gamePage.classList.remove('hidden');
+  startPage.classList.add('hidden');
+});
