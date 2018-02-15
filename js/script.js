c06e1f4a70fc2fa8e239e858545614fce577e7e6
Vyacheslav <thirtylynx@gmail.com>
2018-02-15 11:21:40 +0500

    Функция случайного отбора 9-ти карт

--- js/script.js
+++ js/script.js
@@ -8,3 +8,76 @@ startButton.addEventListener('click', function() {
   gamePage.classList.remove('hidden');
   startPage.classList.add('hidden');
 });
+
+// сортировка
+
+var cards = [
+  '0C',
+  '0D',
+  '0H',
+  '0S',
+  '2C',
+  '2D',
+  '2H',
+  '2S',
+  '3C',
+  '3D',
+  '3H',
+  '3S',
+  '4C',
+  '4D',
+  '4H',
+  '4S',
+  '5C',
+  '5D',
+  '5H',
+  '5S',
+  '6C',
+  '6D',
+  '6H',
+  '6S',
+  '7C',
+  '7D',
+  '7H',
+  '7S',
+  '8C',
+  '8D',
+  '8H',
+  '8S',
+  '9C',
+  '9D',
+  '9H',
+  '9S',
+  'AC',
+  'AD',
+  'AH',
+  'AS',
+  'JC',
+  'JD',
+  'JH',
+  'JS',
+  'KC',
+  'KD',
+  'KH',
+  'KS',
+  'QC',
+  'QD',
+  'QH',
+  'QS'
+];
+
+// отбор 9 случайных карт
+
+function compareRandom() {
+  return Math.random() - 0.5;
+}
+function mixRandomArray(array) {
+  var uniqueCards = 9;
+  var mixedArray = array.sort(compareRandom);
+  var cutArray = mixedArray.slice(0, uniqueCards);
+  return cutArray;
+}
+
+mixRandomArray(cards);
+
+var mas = mas1;
