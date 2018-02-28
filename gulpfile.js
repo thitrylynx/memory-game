3450567877264c948f6bd54314b7a6d07bd589a5
Vyacheslav <thirtylynx@gmail.com>
2018-02-28 21:29:23 +0500

    Оптимизация, новый блок, gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+  gulp
+    .src('sass/style.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('build/css'));
+});
